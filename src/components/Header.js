3b53cbcd9f213ca42cc7bf00c547ea2f436ee00a
Laisvunas <saugos@gmail.com>
2022-02-14 14:31:36 +0200

    init

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+  return (
+    <header className="App-header">
+      <ul className="container">
+        <li key="home">
+          <Link to="/">My Site</Link>
+        </li>
+        <li key="new">
+          <Link to="/new">New Post</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
